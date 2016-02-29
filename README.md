80ad3591609541a69016705e10677d4a44b43e41
Alessandro Maroso <alessandro.maroso@iks.it>
2016-02-29 11:56:31 +0100

    Updated telegram contact.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # TODO
-- fix hover colors
 - implement telegram message send in contacts section
 - write about me section
 - write resume section
--- js/telegram.js
+++ js/telegram.js
@@ -1,14 +1,31 @@
-$( "#contactForm" ).submit(function( event ) {
-  $.get( "https://api.telegram.org/bot190531902:AAGEh0NWXgR37DaoiMVytlJ_iuf4iHPD6r8/sendMessage?chat_id=14174896&text=test", function() {
-    alert( "success" );
-  })
-  .done(function() {
-    alert( "second success" );
-  })
-  .fail(function() {
-    alert( "error" );
-  })
-  .always(function() {
-    alert( "finished" );
-  });
+$("#contactForm").submit(function( event ) {
+  event.preventDefault();
+  var name = $("#contactName").val();
+  var email = $("#contactEmail").val();
+  var message = $("#contactMessage").val();
+  var text = message + " from " + name + " " + email;
+  var chat_id = 14174896;
+  var url = "https://api.telegram.org/bot190531902:AAGEh0NWXgR37DaoiMVytlJ_iuf4iHPD6r8/sendMessage";
+  $('#image-loader').fadeIn();
+  httpGetAsync(url+"?"+"chat_id="+chat_id+"&text="+text, function() {
+    $('#image-loader').fadeOut();
+    $('#message-error').fadeIn();
+  },function (){
+    $('#image-loader').fadeOut();
+    $('#message-success').fadeIn();
+  }
+);
 });
+
+function httpGetAsync(url, error, success) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.status == 200) {
+      success();
+    } else {
+      error();
+    }
+  };
+  xmlHttp.open("GET", url, true); // true for asynchronous
+  xmlHttp.send(null);
+}
